32c35c7fad80ed486a5789743e2fdec902750983
MungaiKeren <wambukeren@gmail.com>
2025-02-22 08:50:06 +0300

    user model

---
+++ user.py
@@ -0,0 +1,10 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, TIMESTAMP, text
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()')) 
